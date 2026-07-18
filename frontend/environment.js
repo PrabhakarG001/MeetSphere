f2c89c7e59505aeae9d3819b4d382bf1c1d6a272
PrabhakarG001 <Prabhakarg465@gmail.com>
2026-07-18 18:46:58 +0530

    Update default backend URL to point to production

--- frontend/environment.js
+++ frontend/environment.js
@@ -1,3 +1,3 @@
-const server = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
+const server = import.meta.env.VITE_BACKEND_URL || "https://meetsphere-0syn.onrender.com";
 
 export default server;
